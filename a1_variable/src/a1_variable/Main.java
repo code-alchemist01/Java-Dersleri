55bb05eb5e71a05c3572849cc6885ef5145dd22a
kutay <ibrahimkutaysahin765@gmail.com>
2024-10-13 16:00:58 +0300

    a1 eklendi

---
+++ a1_variable/src/a1_variable/Main.java
@@ -0,0 +1,28 @@
+package a1_variable;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 25;
+		double b = 23.3;
+		byte c = 12;
+		short d = 122;
+		long e = 12751987;
+		char f = 'k';
+		float g = (float) 12.2;
+		String h = "kutay";
+		boolean ı = true;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		System.out.println(ı);
+
+	}
+
+}
