6cc8d53ac63a932ea33381b5e9940ae5ba04559e
kutay <ibrahimkutaysahin765@gmail.com>
2024-10-14 22:24:11 +0300

    .

---
+++ b7_For/src/b7_For/Main.java
@@ -0,0 +1,19 @@
+package b7_For;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/* 
+		 
+		 for (başlangıç; koşul; arttırma veya azaltma;
+		 	Koşul Doğru olduğu sürece for işlemi devam eder 
+		 */
+		
+		for(int i = 0; i < 100; i++) {
+			System.out.println("Kutay Aslıyı Seviyor");
+		}
+		
+
+	}
+
+}
---
+++ b8_While/src/b8_While/Main.java
@@ -0,0 +1,27 @@
+package b8_While;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+
+			while(koşul) {
+			
+				koşul doğru olduğu sürece çalışır
+				
+				}
+
+			While genelde döngünün ne kadar süreceğinin kesin belli olmadığı durumlarda for ise daha çok sayma yani belirgin ifadelerde kullanılır
+		
+		*/
+		
+		int i = 0;
+		
+		while(i < 100) {
+			System.out.println("Kutay Aslıyı Seviyor");
+			i++;
+		}
+
+	}
+
+}
